84c3903fa736c74a1ca8392832fec5e58c8d4414
Anatoli <PanasAA@web.de>
2025-02-17 21:41:47 +0100

    Added DBConfig

--- db/db_configs.py
+++ db/db_configs.py
@@ -1,6 +1,7 @@
 import os, ast
 from dotenv import load_dotenv
 from pymysql.cursors import DictCursor
+from typing import Optional, Dict, List
 
 class DBConfig:
     def __init__(self):
@@ -8,20 +9,24 @@ class DBConfig:
         self._all_name_configs = self._set_all_name_configs()
 
 
-    def _set_all_name_configs(self) -> list[str]:
+    def _set_all_name_configs(self) -> List[str]:
         env_variables = [key for key in os.environ.keys() if key.startswith('DBCONFIG_')]
         return env_variables
 
-    def get_config(self, db_name: str = None, cursor_class: bool=True) -> dict | None:
+    def get_config(self, db_name: Optional[str] = None, cursor_class: bool=True) -> Optional[Dict]:
         if db_name in self._all_name_configs:
             dbconfig_str = os.getenv(db_name)
-            dbconfig_dict = ast.literal_eval(dbconfig_str)
-            if cursor_class:
-                dbconfig_dict['cursorclass'] = DictCursor
-            return dbconfig_dict
+            if dbconfig_str:
+                try:
+                    dbconfig_dict = ast.literal_eval(dbconfig_str)
+                    if cursor_class:
+                        dbconfig_dict['cursorclass'] = DictCursor
+                    return dbconfig_dict
+                except (ValueError, SyntaxError) as e:
+                    print(f"Ошибка, при попытке преобразовать конфигурацию {db_name} в словарь: {e}")
         return None
 
-    def get_all_name_configs(self) -> list[str]:
+    def get_all_name_configs(self) -> List[str]:
         return self._all_name_configs
 
 # db_conf = DBConfig()
