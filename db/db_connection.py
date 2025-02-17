3898bc21fa5a10b0613d909c57fd4e02ced0d063
Anatoli <PanasAA@web.de>
2025-02-17 22:01:01 +0100

    Added db_connection.py

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,28 +1,41 @@
 import pymysql
+from pymysql.cursors import Cursor
+from pymysql.connections import Connection
+from typing import Optional, Dict
 
 class DBConnector:
 
-    def __init__(self, dbconfig: dict):
-        self._dbconfig = dbconfig
-        self._connection = self._set_connection()
-        self._cursor = self._set_cursor()
+    def __init__(self, dbconfig: Dict):
+        self._dbconfig: Dict = dbconfig
+        self._connection: Optional[Connection] = self._set_connection()
+        self._cursor: Optional[Cursor] = self._set_cursor()
 
-    def _set_connection(self):
-        connection = pymysql.connect(**self._dbconfig)
-        return connection
+    def _set_connection(self) -> Optional[Connection]:
+        try:
+            connection: Connection = pymysql.connect(**self._dbconfig)
+            return connection
+        except pymysql.MySQLError as e:
+            print(f"Ошибка подключения к БД: {e}")
+            return None
 
-    def _set_cursor(self):
-        cursor = self._connection.cursor()
-        return cursor
+    def _set_cursor(self) -> Optional[Cursor]:
+        if self._connection:
+            try:
+                cursor: Cursor = self._connection.cursor()
+                return cursor
+            except Exception as e:
+                print(f"Ошибка курсора: {e}")
+            return None
 
-    def get_connection(self):
+    def get_connection(self) -> Optional[Connection]:
         return self._connection
 
-    def get_cursor(self):
+    def get_cursor(self) -> Optional[Cursor]:
         return self._cursor
 
     def close(self):
         if self._connection.open:
             self._cursor.close()
             self._connection.close()
+            print("Подключение к БД и курсор закрыты.")
 
