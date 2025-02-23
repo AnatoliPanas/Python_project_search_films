f7f48dc1b5135deae7b51db6d583c53b9df890b7
Anatoli <PanasAA@web.de>
2025-02-23 12:41:30 +0100

    Modif db_queries_manager.py

--- db/db_queries_manager.py
+++ db/db_queries_manager.py
@@ -1,15 +1,18 @@
-import pymysql
+import pymysql, logging
 from pymysql.cursors import DictCursor
 from db.db_connection import DBConnector
 from typing import List, Tuple, Optional, Dict
 
 from db.sql_queries import FilmQueries, SearchCriteriaFilm
 
+logging.basicConfig(level=logging.INFO)
+# Чтобы знать с какого модуля сообщение
+logger = logging.getLogger(__name__)
 
 class LoggingDictCursor(DictCursor):
-    def execute(self, query, params: Tuple = ()):
+    def execute(self, query: str, params: Tuple = ()):
 
-        print(f"Выполняется запрос: {query} с параметрами {params}")
+        logger.info(f"Выполняется запрос: {query} с параметрами {params}")
         # try:
             # log_query = """
             # log_query = SearchCriteriaFilm.INSERT_CRITERIAFILM
@@ -17,8 +20,10 @@ class LoggingDictCursor(DictCursor):
             # self._connection.commit()
         # except pymysql.MySQLError as e:
         #     print(f"Ошибка при записи лога в базу данных: {e}")
-
-        return super().execute(query, params)
+        try:
+            return super().execute(query, params)
+        except pymysql.MySQLError as e:
+            logger.error(f"Ошибка выполнения запроса: {e}")
 
 class DBQueriesManager(DBConnector):
     def __init__(self, dbconfig):
@@ -32,49 +37,47 @@ class DBQueriesManager(DBConnector):
             records = cursor.fetchall()
             return records
         except pymysql.MySQLError as e:
-            print(f"Ошибка выполнения запроса: {e}")
+            logger.error(f"Ошибка выполнения запроса [get_records]: {e}")
             return None
 
-    def get_record(self, query, params = ()) -> Optional[Dict]:
+    def get_record(self, query: str, params = ()) -> Optional[Dict]:
         cursor = self.get_cursor()
         try:
             cursor.execute(query, params)
             record = cursor.fetchone()
             return record
         except pymysql.MySQLError as e:
-            print(f"Ошибка выполнения запроса: {e}")
+            logger.error(f"Ошибка выполнения запроса [get_record]: {e}")
             return None
 
-    def execute_ins_upd_del(self, query, params: Tuple = ()) -> bool:
+    def execute_ins_upd_del(self, query: str, params: Tuple = ()) -> bool:
         cursor = self.get_cursor()
         try:
             cursor.execute(query, params)
             self.commit()
             return True
         except pymysql.MySQLError as e:
-            print(f"Ошибка выполнения запроса: {e}")
+            logger.error(f"Ошибка выполнения запроса [execute_ins_upd_del]: {e}")
             self.rollback()
             return False
 
     def get_bd_name(self):
         return self._dbconfig.get("database")
 
-    def get_converting_SQLquery(self, query: str, *args ):
+    def get_converting_SQLquery(self, query: str, *args) -> Tuple[str, List]:
         placeholders = []
+        params = []
+
         for arg in args:
             if arg and arg != [''] and isinstance(arg, list):
                 placeholder = ', '.join(['%s'] * len(arg))
-                if placeholder:
-                    placeholders.append(placeholder)
+                placeholders.append(placeholder)
+                params.extend(arg)
             elif isinstance(arg, dict):
-                query = query.format(*arg.get('s'))
+                query = query.format(**arg)
             elif isinstance(arg, str):
-                placeholder = '%s'
-                if placeholder:
-                    placeholders.append(placeholder)
+                placeholders.append('%s')
+                params.append(arg)
 
-        query = query % tuple(placeholders)
-
-        # params = [item for arg in args for item in arg if arg != [''] and isinstance(arg, list)]
-        params = [item for arg in args for item in arg if arg != ['']]
+        query = query % tuple(placeholders) if placeholders else query
         return query, params
